17d69a01789c30e84886762e2bc55532db84081c
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2023-11-04 13:42:53 +0700

    first commit

---
+++ src/main/java/ThreeWaterJugProblem/Path.java
@@ -0,0 +1,45 @@
+package ThreeWaterJugProblem;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Path<T> {
+    private List<T> path;
+    
+    public Path(){
+        path = new ArrayList<>();
+    }
+    
+    public void addVertex(T vertex) {
+        path.add(vertex);
+    }
+    
+    public List<T> getPath(){
+        return path;
+    }
+    
+    public void setPath(List<T> path){
+        this.path.addAll(path);
+    }
+
+    public void printPath(){
+        for (int i = 0; i < path.size(); i++){
+            System.out.print(path.get(i).toString() + " ");
+        }
+        
+        System.out.println();
+    }    
+    public void printPathAsGraph() {
+        for (int i = 0; i < path.size(); i++) {
+            Vertex currentVertex = (Vertex) path.get(i);
+            System.out.print(currentVertex + " -> ");
+
+            // Print an arrow (or edge) between vertices
+            if (i < path.size() - 1) {
+                System.out.print("-> ");
+            }
+        }
+
+        System.out.println();
+    }
+}
